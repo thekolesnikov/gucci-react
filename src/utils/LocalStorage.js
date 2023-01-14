289a482970e9ef08a83568b46a5e251f0566486e
thekolesnikov <thesharkov@gmail.com>
2023-01-14 15:46:22 +0200

    build version

--- src/utils/LocalStorage.js
+++
@@ -1,7 +0,0 @@
-export function addToLocalStorage(arr) {
-    localStorage.setItem('shoes', JSON.stringify(arr));
-}
-
-export function getFromLocalStorage() {
-    return JSON.parse(localStorage.getItem('shoes'));
-}
